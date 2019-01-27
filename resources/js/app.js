980da3167d5fc23fa7a35f8da000dae1bc7d3697
Endy Hardy <endy0611@gmail.com>
2019-01-27 13:58:02 +0800

    Add Firebase integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,11 @@
-require('./bootstrap');
-require('./ReactApp');
+require("./bootstrap");
+require("./ReactApp");
+
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAnBd-7hmPO0dtJLrtGDWRF5wHbmiU0ho4",
+  authDomain: "entree-70ebf.firebaseapp.com",
+  databaseURL: "https://entree-70ebf.firebaseio.com",
+  storageBucket: "entree-70ebf.appspot.com"
+});
